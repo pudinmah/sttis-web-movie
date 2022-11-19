8e5f3fab05808739c86a1f874a8b8b13513ba45e
gilangcahyadi <gilangcahyadi20@gmail.com>
2022-11-20 01:51:59 +0700

    hapus file

--- analisis/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","projek_kampus");
-//Check connection
-if (mysqli_connect_error()){
-    echo "Failed to connect to Mysql" . mysqli_connect_error();
-    exit();
-}
-
-?>
